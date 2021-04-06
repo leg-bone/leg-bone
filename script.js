23881104224743fda5e146db6514c606475f80a5
charlie <wct257@nyu.edu>
2021-04-06 15:01:28 -0400

    d

---
+++ script.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+        (function changeImage() {
+            var rotator = document.getElementById('rotator');  
+            var imageDir = 'D:\Desktop\webDev';
+            var delayInSeconds = 5;
+
+            var images = ['kat1.jpg', 'kat2.jpg', 'kat3.jpg', 'kat4.jpg', 'kat5.jpg'];
+
+            var num = 0;
+            var changeImage = function() {
+                var len = images.length;
+                rotator.src = imageDir + images[num++];
+                if (num == len) {
+                    num = 0;
+                }
+            };
+
+        })();
+        </script>
