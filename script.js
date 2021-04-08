6067de358815a7e5d86f2fc6733a931265251f5d
charlie <wct257@nyu.edu>
2021-04-08 02:45:05 -0400

    s

--- script.js
+++ script.js
@@ -17,9 +17,5 @@
             };
 
         })();
-        (function randomNum() {
-          var x = Math.floor(Math.random()* 999999999999999999) + 100000000000000000;
-        })();
-        randomNum();
-        document.write("<p>YOU ARE IN VIOLATION OF DIGITAL LAW. A.I. TECHNOLOGY HAS IDENTIFIED YOU AS USER_"x" THROUGH VIDEO SURVEILLANCE. WE HAVE AN OUTSTANDING WARRANT FOR YOUR ARREST. STAY WHERE YOU ARE UNTIL WE ARRIVE. DO NOT RESIST.</p>");
+
         </script>
---
+++ scriptA.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+        (function randomNum() {
+          var x = Math.floor(Math.random()* 999999999999999999) + 100000000000000000;
+        })();
+        randomNum();
+        document.write("<p>YOU ARE IN VIOLATION OF DIGITAL LAW. A.I. TECHNOLOGY HAS IDENTIFIED YOU AS USER_"x" THROUGH VIDEO SURVEILLANCE. WE HAVE AN OUTSTANDING WARRANT FOR YOUR ARREST. STAY WHERE YOU ARE UNTIL WE ARRIVE. DO NOT RESIST.</p>");
+        </script>
