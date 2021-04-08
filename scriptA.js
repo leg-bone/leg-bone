1afc6225d0d79de371ad40711587ef2b14f9405d
charlie <wct257@nyu.edu>
2021-04-08 03:14:43 -0400

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -1,6 +1,7 @@
 
         function randomNum() {
           var x = Math.floor(Math.random()* 999999999999999999) + 100000000000000000;
+          console.log(x);
         }
         randomNum();
         document.write("<p>YOU ARE IN VIOLATION OF DIGITAL LAW. A.I. TECHNOLOGY HAS IDENTIFIED YOU AS USER_"+x+" THROUGH VIDEO SURVEILLANCE. WE HAVE AN OUTSTANDING WARRANT FOR YOUR ARREST. STAY WHERE YOU ARE UNTIL WE ARRIVE. DO NOT RESIST.</p>");
